084a2aee7dd3caf21d585885b9c67a553917aa52
Louis Pixodeo <louis@pixodeo.net>
2023-06-12 09:17:48 +0700

    update banner

--- javascripts/discourse/widgets/banner-buttons.js
+++ javascripts/discourse/widgets/banner-buttons.js
@@ -1,11 +1,15 @@
 import { createWidget } from "discourse/widgets/widget";
-import hbs from "discourse/widgets/hbs-compiler";
+import RenderGlimmer from "discourse/widgets/render-glimmer";
+import { hbs } from "ember-cli-htmlbars";
 
 export default createWidget("banner-buttons", {
-  tagName: "div.search-banner-buttons",
-
-  template: hbs`
-    <div class="search-banner-buttons">
+  tagName: "div.search-banner-wrap",
+  html() {
+    return [
+      new RenderGlimmer(
+        this,
+        "div",
+        hbs`<div class="search-banner-buttons">
         <DButton
           @action={{action "showCreateAccount"}}
           @label="sign_up"
@@ -17,6 +21,8 @@ export default createWidget("banner-buttons", {
           @label="log_in"
           @class="btn-primary"
         />
-      </div>;
-  `
+      </div>`
+      ),
+    ];
+  },
 });
